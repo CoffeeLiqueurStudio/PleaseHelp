3273d002fd652b5773022630f3f0745fc9d9c6ec
CoffeeLiqueurStudio <coffeeliqueurstudio@gmail.com>
2025-06-30 18:31:37 +0400

    Revert "tryed dialogue"
    This reverts commit 17e6c2ea60007ba2c1cb9c0cd55155423d35f0a4.

--- Code/Player/player.gd
+++ Code/Player/player.gd
@@ -1,26 +1,14 @@
 extends CharacterBody2D
 @onready var label: Label = $Label
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 var speed: float = 160.0
 enum States {IDLE, RUN}
 var state: States = States.IDLE
-var playerdirection: String = "down"
+
 func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = input_dir * speed
 	
-	if input_dir == Vector2(1,0):
-		playerdirection = "right"
-	elif input_dir == Vector2(-1,0):
-		playerdirection = "left"
-		
-	if input_dir == Vector2(0,-1):
-		playerdirection = "up"
-	elif input_dir == Vector2(0,1):
-		playerdirection = "down"
-		
-
 	if velocity == Vector2.ZERO:
 		state = States.IDLE
 	elif velocity != Vector2.ZERO:
@@ -29,6 +17,6 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 func switch_state():
 	if state == States.IDLE:
-		animated_sprite_2d.play(str(playerdirection)+"_idle")
+		label.text = "Player state: Idle"
 	if state == States.RUN:
-		animated_sprite_2d.play(str(playerdirection)+"_walk")
+		label.text = "Player state: Run"
